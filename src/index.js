1259d4404c8714e0d061605f714e18d0a4764eac
xh-chen <1078937456@qq.com>
2017-12-12 16:27:45 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2017-12-12 11:36:56
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-12-12 16:02:41
+*/
+require('./css/common.css');
+require('./css/c.css');
